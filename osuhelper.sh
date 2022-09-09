46bc5adcad721860ed225c00f0a3c8ef7e1fff1d
neil mehra <neilmehra@outlook.com>
2022-09-09 01:10:30 -0400

    works

--- osuhelper.sh
+++ osuhelper.sh
@@ -6,19 +6,29 @@ downloaddir=$HOME/downloads
 songdir=$osudir/Songs
 skindir=$osudir/Skins
 
-for file in $downloaddir/*.osz; do
-  mv "$file" $songdir
-done
+song_count=`cd $downloaddir; ls | grep '\.osz' | wc -l`
+skin_count=`cd $downloaddir; ls | grep '\.osk' | wc -l`
 
-for file in $downloaddir/*.osk; do
-  filename=$(basename -- "$file")
-  no_ext_name="${filename%.*}"
-  mkdir ./"$no_ext_name"
-  mv ./"$filename" ./"$no_ext_name"
-  cd ./"$no_ext_name"
-  7z x ./"$filename" > /dev/null 2>&1
-  rm ./"$filename"
-  cd ..
-  mv ./"$no_ext_name" $skindir
-done
+if [[ $song_count != '0' ]]; then
+  echo 'Song(s) found!'
+  for file in $downloaddir/*.osz; do
+    mv "$file" $songdir
+  done
+fi
+
+if [[ $skin_count != '0' ]]; then
+  echo 'Skin(s) found!'
+  for file in $downloaddir/*.osk; do
+    filename=$(basename -- "$file")
+    base="${filename%.*}"
+    mkdir $downloaddir/"$base"
+    mv $downloaddir/"$filename" $downloaddir/"$base"
+    cd $downloaddir/"$base"
+    7z x $downloaddir/"$filename" > /dev/null 2>&1
+    rm $downloaddir/"$base"/"$filename"
+    cd $downloaddir
+    mv $downloaddir/"$base" $songdir
+  done
+fi
+
 
